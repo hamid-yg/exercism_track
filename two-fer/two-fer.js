8e775494af01d9cf786e4d2f5ac481b7761bd847
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-07-26 14:56:46 +0100

    feature: second exercice

---
+++ two-fer/two-fer.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (name) => {
+  if (name) {
+    return `One for ${name}, one for me.`;
+  } else {
+    return "One for you, one for me.";
+  }
+};
