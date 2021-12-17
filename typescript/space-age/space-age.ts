fb1fb79ce5fb415feec291ad2c271f349985122e
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-12-17 09:54:20 +0100

    add space-age

---
+++ typescript/space-age/space-age.ts
@@ -0,0 +1,14 @@
+export function age(planet: string, seconds: number): number {
+
+}
+
+export const PLANET: string[] = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune',
+];
